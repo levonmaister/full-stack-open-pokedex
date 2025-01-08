4b5c2999bb5136aa0560b9f039c61a16e0b2701c
Levon Lopez-Eknosyan <thecactusplayer8@gmail.com>
2025-01-08 09:28:45 +0200

    -

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -30,9 +30,13 @@ const PokemonPage = ({ previous, next }) => {
   return (
     <>
       <div className="links">
+
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
+        
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
+
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
