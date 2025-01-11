16561ce4986d5dfd4da303ebb0dd803622985e5d
Levon Lopez-Eknosyan <thecactusplayer8@gmail.com>
2025-01-11 13:33:37 +0200

    deployment update

--- e2e-tests/therealdeal.spec.js
+++ e2e-tests/therealdeal.spec.js
@@ -6,4 +6,12 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('Navigation from main page', async ({page}) => {
+    await page.goto('');
+    await page.getByText('ivysaur').click();
+    await expect(page.getByText('chlorophyll')).toBeVisible();
+  })
+
 })
+
