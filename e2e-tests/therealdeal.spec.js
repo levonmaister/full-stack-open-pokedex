dcfe61fbe89a96a6bd82a518066826246cbda034
Levon Lopez-Eknosyan <thecactusplayer8@gmail.com>
2025-01-11 13:34:53 +0200

    eslint failure

--- e2e-tests/therealdeal.spec.js
+++ e2e-tests/therealdeal.spec.js
@@ -7,10 +7,10 @@ describe('Pokedex', () => {
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 
-  test('Navigation from main page', async ({page}) => {
-    await page.goto('');
-    await page.getByText('ivysaur').click();
-    await expect(page.getByText('chlorophyll')).toBeVisible();
+  test('Navigation from main page', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 
 })
