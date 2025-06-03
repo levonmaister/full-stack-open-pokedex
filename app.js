63887044f0dfe13ed57469e4fbf4a5ea9b5da9aa
levonmaister <levon.lopezeknosyan@aalto.fi>
2025-06-03 10:02:39 +0300

    broken health check2

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw('error...  ')
-  res.send('ok')
+  res.status(200).send('OK')
 })
 
 app.listen(PORT, () => {
